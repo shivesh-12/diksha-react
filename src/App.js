8c862f8cab0a33f0731f92884100e1ce080cf882
unknown <shiveshr9@gmail.com>
2023-06-04 18:27:52 +0530

    first

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>I am diksha </h1>
+      <h1>I am diksha rajora edited by local system to test git </h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
